3e0b8abc4a500339726a6a680f4b471f4627e88a
Alexey Martinez <amartinez@Encore.local>
2023-04-05 15:25:02 -0400

    react lists section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
 
-// const title = "React";
+// yconst title = "React";
 const welcome = {
   title: "React",
-  greeting: "Hey",
+  greeting: "Hello",
 };
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
@@ -12,8 +31,23 @@ function App() {
       <h1>
         {welcome.greeting} {welcome.title}{" "}
       </h1>
+      <h1> My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> Author: {item.author},</span>
+            <span> # of comments: {item.num_comments},</span>
+            <span> points: {item.points}</span>
+              
+            </li>);
+        })}
+      </ul>
     </div>
   );
 }
